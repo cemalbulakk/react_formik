24f682f29b5a276179c0ac3794f6052c6785dc56
Cemal Salih Bulak <cemal.bulak@digilera.com>
2022-11-20 15:11:03 +0300

    Kurulum

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Formik, Field, Form } from "formik";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Sign Up</h1>
+      <Formik
+        initialValues={{
+          firstName: "",
+          lastName: "",
+          email: "",
+        }}
+        onSubmit={async (values) => {
+          console.log(JSON.stringify(values, null, 2));
+        }}
+      >
+        <Form>
+          <div className="row">
+            <div className="col">
+              <label htmlFor="firstName">First Name</label>
+            </div>
+            <div className="col">
+              <Field id="firstName" name="firstName" placeholder="Jane" />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <label htmlFor="lastName">Last Name</label>
+            </div>
+            <div className="col">
+              <Field id="lastName" name="lastName" placeholder="Doe" />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <label htmlFor="email">Email</label>
+            </div>
+            <div className="col">
+              <Field
+                id="email"
+                name="email"
+                placeholder="jane@acme.com"
+                type="email"
+              />
+            </div>
+          </div>
+
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
     </div>
   );
 }
